dcc657edbae29de1d9f954bc8a5e546054e4ccd1
Upritik <117970072+Upritik@users.noreply.github.com>
2025-06-12 12:56:05 +0530

    Create AverageMarks.java

---
+++ Practice_Day1/AverageMarks.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class AverageMarks{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int maths=94;
+        int physics=95;
+        int chemistry=96;
+        int average=(maths+physics+chemistry)/3;
+        System.out.println("Average Marks: "+average);
+    }
+}
