f8f6fbc22f6ed5e760d3c7bdcc7c3c19e6b276e2
Upritik <117970072+Upritik@users.noreply.github.com>
2025-06-12 12:22:02 +0530

    Create age_calculator.java

---
+++ Practice_Day1/age_calculator.java
@@ -0,0 +1,8 @@
+public class age_calculator {
+    public static void main(String[] args){
+        int birthyear=2000;
+        int currentyear=2024;
+        int age=currentyear-birthyear;
+        System.out.println("Harry's age is "+age);
+    }
+}
