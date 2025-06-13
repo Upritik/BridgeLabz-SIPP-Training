a3fb191e7872cb8963eb08fc05c60b249d4cdd42
Upritik <117970072+Upritik@users.noreply.github.com>
2025-06-13 09:26:18 +0530

    Create KmtoMiles.java

---
+++ Day_2/KmtoMiles.java
@@ -0,0 +1,9 @@
+package Day_2;
+
+public class KmtoMiles {
+    public static void main(String[] args){
+        double km=10.8;
+        double miles = km*1.6;
+        System.out.println(km+" km is equal to "+ miles + "miles");
+    }
+}
