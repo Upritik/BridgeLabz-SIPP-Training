59cce2bed4052acb096af237b5c71e6d9cd43cd1
Upritik <117970072+Upritik@users.noreply.github.com>
2025-06-19 12:47:57 +0530

    Create PowerofNumber.java

---
+++ Day4_Arrays/PowerofNumber.java
@@ -0,0 +1,14 @@
+package Day4_Arrays;
+import java.util.*;
+public class PowerofNumber {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        int power=sc.nextInt();
+        int result=1;
+        for(int i=1;i<=power;i++){
+            result=result*num;
+        }
+        System.out.println(result);
+    }
+}
