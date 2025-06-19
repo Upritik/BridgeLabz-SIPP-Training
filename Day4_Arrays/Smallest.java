ec4125581c08abcec6ca609385c775f39eff35ff
Upritik <117970072+Upritik@users.noreply.github.com>
2025-06-19 11:53:34 +0530

    Create Smallest.java

---
+++ Day4_Arrays/Smallest.java
@@ -0,0 +1,17 @@
+package Day4_Arrays;
+import java.util.*;
+public class Smallest {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        if(a<b && a<c){
+            System.out.println("A is Smaller");
+        }else if(b<a && b<c){
+            System.out.println("B is Smaller");
+        }else{
+            System.out.println("C is Smaller");
+        }
+    }
+}
