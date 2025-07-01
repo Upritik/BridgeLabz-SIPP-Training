78d074c769e853d191a5ab8cf6b2392c6bf4f320
Upritik <117970072+Upritik@users.noreply.github.com>
2025-07-01 09:28:55 +0530

    Create Calculator.java

---
+++ Day4_Arrays/Calculator.java
@@ -0,0 +1,34 @@
+package Day4_Arrays;
+import java.util.*;
+public class Calculator {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        char operator = sc.next().charAt(0);
+        int b = sc.nextInt();
+        switch(operator){
+            case '+':
+            System.out.println(a + b);
+            break;
+            case'-':
+            System.out.println(a-b);
+            break;
+            case'*':
+            System.out.println(a*b);
+            break;
+            case'/':
+            if(b != 0){
+                System.out.println(a/b);
+            }else{
+                System.out.println("0");
+            }
+            break;
+            case'%':
+            System.out.println(a%b);
+            break;
+            default:
+            System.out.println("Invalid operator");
+            break;
+        }
+    }
+}
