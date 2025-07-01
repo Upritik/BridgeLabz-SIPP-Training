3a90457f94dcc74372e4a237137acf7b8a519294
Upritik <117970072+Upritik@users.noreply.github.com>
2025-07-01 09:28:31 +0530

    Create LargerAndSmaller.java

---
+++ Day4_Arrays/LargerAndSmaller.java
@@ -0,0 +1,21 @@
+package Day4_Arrays;
+import java.util.*;
+public class LargerAndSmaller {
+    public static void main(String[] args){
+        int[] arr={34,4,6,11,-77,23,98,76};
+        int max=arr[0];
+        int min=arr[0];
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>max){
+                max=arr[i];
+                i++;
+            }
+            else if(arr[i]<min){
+                min=arr[i];
+            }
+        }
+        System.out.println(max);
+        System.out.println(min);
+
+    }
+}
