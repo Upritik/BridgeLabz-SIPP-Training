194bed41ac3314047e55bc773c9a4360bc63f191
Upritik <117970072+Upritik@users.noreply.github.com>
2025-07-01 09:28:06 +0530

    Create problem_14.java

---
+++ Day4_Arrays/problem_14.java
@@ -0,0 +1,14 @@
+package Day4_Arrays;
+import java.util.*;
+public class problem_14 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String[] names={"Amar","Akbar","Anthony"};
+        int[] ages=new int[3];
+        int[] heights=new int[3];
+        for(int i=0;i<3;i++){
+            ages[i]=sc.nextInt();
+            heights[i]=sc.nextInt();
+        }
+    }
+}
