e4c30cee1c5bcd9c34cfaf36a3f6693c480b06c9
Upritik <117970072+Upritik@users.noreply.github.com>
2025-08-17 18:02:23 +0530

    Create HotelBooking.java

---
+++ JavaConstructor/Level 1/HotelBooking.java
@@ -0,0 +1,34 @@
+public class HotelBooking {
+    String guestName;
+    String roomType;
+    int nights;
+
+    public HotelBooking() {
+        this.guestName = "Unknown";
+        this.roomType = "Standard";
+        this.nights = 1;
+    }
+
+    public HotelBooking(String guestName, String roomType, int nights) {
+        this.guestName = guestName;
+        this.roomType = roomType;
+        this.nights = nights;
+    }
+
+    public HotelBooking(HotelBooking booking) {
+        this.guestName = booking.guestName;
+        this.roomType = booking.roomType;
+        this.nights = booking.nights;
+    }
+
+    public void displayBooking() {
+        System.out.println("Guest: " + guestName + ", Room: " + roomType + ", Nights: " + nights);
+    }
+
+    public static void main(String[] args) {
+        HotelBooking b1 = new HotelBooking("Neffex", "Deluxe", 3);
+        HotelBooking b2 = new HotelBooking(b1);
+        b1.displayBooking();
+        b2.displayBooking();
+    }
+}
