676dfcd979a4978e528eaeaf87216a2312ab972a
Upritik <117970072+Upritik@users.noreply.github.com>
2025-08-18 10:20:54 +0530

    Create SortStack.java

---
+++ StackAndQueue/SortStack.java
@@ -0,0 +1,34 @@
+package StackAndQueue;
+
+import java.util.Stack;
+
+public class SortStack {
+    public static void insertSorted(Stack<Integer> stack, int element) {
+        if (stack.isEmpty() || stack.peek() <= element) {
+            stack.push(element);
+            return;
+        }
+        int top = stack.pop();
+        insertSorted(stack, element);
+        stack.push(top);
+    }
+    public static void sortStack(Stack<Integer> stack) {
+        if (stack.isEmpty()) {
+            return;
+        }
+        int top = stack.pop();
+        sortStack(stack);
+        insertSorted(stack, top);
+    }
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(3);
+        stack.push(1);
+        stack.push(4);
+        stack.push(2);
+        
+        System.out.println("Original stack: " + stack);
+        sortStack(stack);
+        System.out.println("Sorted stack: " + stack);
+    }
+}
